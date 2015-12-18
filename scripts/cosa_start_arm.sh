d4625f36abbcf91b45f78105dd62cd65b86d5d5e
Justin Laue <justin@fp-x.com>
2015-12-17 21:15:04 -0700

    cosa_start_arm.sh cleanup
    Change-Id: If00ce7d1702318a29766760041ca5140c18a88fc

--- scripts/cosa_start_arm.sh
+++ scripts/cosa_start_arm.sh
@@ -1,21 +1,3 @@
-##########################################################################
-# If not stated otherwise in this file or this component's Licenses.txt
-# file the following copyright and licenses apply:
-#
-# Copyright 2015 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
 #######################################################################
 #   Copyright [2014] [Cisco Systems, Inc.]
 # 
@@ -31,167 +13,134 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #######################################################################
-
-BINPATH="/usr/bin"
-
-if [ "x"$1 = "xkill" ] || [ "x"$2 = "xkill" ]; then
-	killall ccspRecoveryManager
-	sleep 3
-	killall CcspTandDSsp
-	killall CcspDnsSsp
-	killall CcspFuSsp
-	killall CcspSsdSsp
-	killall CcspPandMSsp
-	killall CcspTr069PaSsp
-	killall PsmSsp
-	killall CcspRmSsp
-	killall CcspCrSsp
-#    killall CcspLmSsp
-	killall CcspMtaAgentSsp
-	killall CcspCMAgentSsp
-    killall CcspLMLite
-	killall webpa
-fi
-
-#
-#	Allow custom plug-ins
-#
-if [ -f "$PWD/cosa_start_custom_1.sh" ]; then
-	./cosa_start_custom_1.sh
-fi
-
-export LD_LIBRARY_PATH=$PWD:.:$PWD/../../lib:$PWD/../../.:/lib:/usr/lib:$LD_LIBRARY_PATH
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
-export LOG4C_RCPATH=/fss/gw/rdklogger
-
-# Check if bbhm has Notify flag present
-NOTIFYPRESENT=`cat /nvram/bbhm_cur_cfg.xml | grep NotifyWiFiChanges`
-REDIRCTEXISTS=""
-
-# If Notify flag is not present then we will add it as per the syscfg DB value
-if [ "$NOTIFYPRESENT" = "" ]
-then
-	REDIRECT_VALUE=`syscfg get redirection_flag`
-	if [ "$REDIRECT_VALUE" = "" ]
-	then
-		#Just making sure if syscfg command didn't fail
-		REDIRCTEXISTS=`cat /nvram/syscfg.db | grep redirection_flag | cut -f2 -d=`
-	fi
-
-	if [ "$REDIRECT_VALUE" = "false" ] || [ "$REDIRCTEXISTS" = "false" ];
-	then
-		
-		echo " Apply Notifywifichanges flse"
-		sed '10 a \ \ \ <Record name=\"eRT.com.cisco.spvtg.ccsp.Device.WiFi.NotifyWiFiChanges\" type=\"astr\">false</Record>' /nvram/bbhm_cur_cfg.xml > /var/tmp/bbhm_cur_cfg.xml
-		sed '10 a \ \ \ <Record name=\"eRT.com.cisco.spvtg.ccsp.Device.WiFi.NotifyWiFiChanges\" type=\"astr\">false</Record>' /nvram/bbhm_bak_cfg.xml > /var/tmp/bbhm_bak_cfg.xml
-		cp /var/tmp/bbhm_cur_cfg.xml /nvram/bbhm_cur_cfg.xml
-		cp /var/tmp/bbhm_bak_cfg.xml /nvram/bbhm_bak_cfg.xml
-		rm /var/tmp/bbhm_cur_cfg.xml
-		rm /var/tmp/bbhm_bak_cfg.xml
-	elif [ "$REDIRECT_VALUE" = "true" ] || [ "$REDIRCTEXISTS" = "true" ];
-	then
-		echo " Apply Notifywifichanges tue"
-		sed '10 a \ \ \ <Record name=\"eRT.com.cisco.spvtg.ccsp.Device.WiFi.NotifyWiFiChanges\" type=\"astr\">true</Record>' /nvram/bbhm_cur_cfg.xml > /var/tmp/bbhm_cur_cfg.xml
-		sed '10 a \ \ \ <Record name=\"eRT.com.cisco.spvtg.ccsp.Device.WiFi.NotifyWiFiChanges\" type=\"astr\">true</Record>' /nvram/bbhm_bak_cfg.xml > /var/tmp/bbhm_bak_cfg.xml
-		cp /var/tmp/bbhm_cur_cfg.xml /nvram/bbhm_cur_cfg.xml
-		cp /var/tmp/bbhm_bak_cfg.xml /nvram/bbhm_bak_cfg.xml
-		rm /var/tmp/bbhm_cur_cfg.xml
-		rm /var/tmp/bbhm_bak_cfg.xml
-	fi
+mykill () {
+    if ps aux | grep $1 | grep -v grep; then
+        echo "killing $1"
+        kill $(ps aux | grep $1 | grep -v grep | awk '{print $2}')
+        sleep 1
+    fi
+}
+# myexec pam CcspPandMSsp "$SubsysArgs"
+myexec() {
+    if [ -e ./$1 ]; then
+        if $3; then; sleep $3; fi
+        cd $1
+        echo $2 $3
+        $2 
+        cd ..
+    fi
+}
+if [ "$1" = "kill" ] || [ "$2" = "kill" ]; then
+    echo Killing CCSP Daemons
+    mykill ccspRecoveryManager
+    mykill CcspTandDSsp
+    mykill CcspDnsSsp
+    mykill CcspFuSsp
+    mykill CcspSsdSsp
+    mykill CcspPandMSsp
+    mykill CcspTr069PaSsp
+    mykill PsmSsp
+    mykill CcspRmSsp
+    mykill CcspCrSsp
+#    mykill CcspLmSsp
+    mykill CcspMtaAgentSsp
+    mykill CcspCMAgentSsp
+    mykill CcspLMLite
+    mykill CcspWecbController   
 fi
 
 # Start coredump
 if [ -f "$PWD/core_compr" ]; then
-	if ! [ -e "/var/core" ]; then
-		mkdir -p /var/core/
-	fi
-	echo "|$PWD/core_compr /var/core %p %e" >/proc/sys/kernel/core_pattern
-	ulimit -c unlimited
+        if ! [ -e "/var/core" ]; then
+                mkdir -p /var/core/
+        fi
+        echo "|$PWD/core_compr /var/core %p %e" >/proc/sys/kernel/core_pattern
+        ulimit -c unlimited
 
-	./core_report.sh &
+        ./core_report.sh &
 fi
 
 # Start Config Recover
-
-#conf_rec &
+echo "Starting Config Recover Daemon ..."
+conf_rec &
 
 cp ccsp_msg.cfg /tmp
 
-# have IP address for dbus config generated
-#./DbusCfg
-$BINPATH/dbus-daemon --config-file=./basic.conf --fork
 
-mkdir -p /var/tmp/logs/
-
-
-if [ -f "/fss/gw/rdklogger/rdkbLogMonitor.sh" ]
-then
-	/fss/gw/rdklogger/rdkbLogMonitor.sh &
+if ps aux | grep dbus-daemon.*ccsp | grep -v grep; then
+    echo "Starting dbus"
+    # have IP address for dbus config generated
+    #./DbusCfg
+    if [ -e ./create_dbus_if.sh ]; then
+        echo "./create_dbus_if.sh"
+        ./create_dbus_if.sh
+    fi
+    dbus-daemon --config-file=./dbus-ccsp.conf --fork
 fi
 
-if [ -f "/fss/gw/rdklogger/fileUploadRandom.sh" ]
-then
-	/fss/gw/rdklogger/fileUploadRandom.sh &
-fi
-touch /tmp/cp_subsys_ert
+#touch cp_subsys_ert
 
-if [ -f /tmp/cp_subsys_ert ]; then
-	Subsys="eRT."
+Subsys="NA"
+SubsysArgs=""
+
+if [ -f ./cp_subsys_ert ]; then
+    Subsys="eRT"
+    SubsysArgs="-subsys eRT."
 elif [ -e ./cp_subsys_emg ]; then
-	Subsys="eMG."
-else
-	Subsys=""
+    Subsys="eMG"
+    SubsysArgs="-subsys eMG."
 fi
 
 echo "Elected subsystem is $Subsys"
 
-if [ "x"$Subsys = "x" ];then
-	$BINPATH/CcspCrSsp
-else
-	echo "$BINPATH/CcspCrSsp -subsys $Subsys"
-	$BINPATH/CcspCrSsp -subsys $Subsys
-fi
+echo "CcspCrSsp $SubsysArgs"
+CcspCrSsp $SubsysArgs
 
-if [ -e ./logagent ]; then
-	cd logagent
+echo "PsmSsp $SubsysArgs"
+PsmSsp $SubsysArgs
 
-	if [ "x"$Subsys = "x" ];then
-		$BINPATH/log_agent
-	else
-		echo "$BINPATH/log_agent -subsys $Subsys"
-		$BINPATH/log_agent -subsys $Subsys
-	fi
-	cd ..
-fi
-
-if [ "x"$Subsys = "x" ];then
-	$BINPATH/PsmSsp
-else
-    echo "$BINPATH/PsmSsp -subsys $Subsys"
-	$BINPATH/PsmSsp -subsys $Subsys
-fi
-
+myexec pam "CcspPandMSsp $SubsysArgs" 1
+# myexec pam "((sh ./email_notification_monitor.sh 12 &) &)" 
 if [ -e ./pam ]; then
-	cd pam
-
-	if [ "x"$Subsys = "x" ];then
-		$BINPATH/CcspPandMSsp
-	else
-		echo "$BINPATH/CcspPandMSsp -subsys $Subsys"
-		$BINPATH/CcspPandMSsp -subsys $Subsys
-	fi
-	cd ..
+    cd ./pam
+    ./email_notification_monitor.sh 12 &
+    disown
+    cd ..
 fi
 
-if [ -e ./wecb ]; then                                                                                                                                                                                                                    
-    cd wecb                                                                                                                
-    if [ "x"$Subsys = "x" ];then                                                                                           
-        $BINPATH/CcspWecbController                                                                                               
-    else                                                                                                                   
-        echo "$BINPATH/CcspWecbController -subsys $Subsys"                                                                        
-       $BINPATH/CcspWecbController -subsys $Subsys                                                                               
-    fi                                                                                                                     
-    cd ..                                                                                                                  
-fi  
+if [ -f ./cp_subsys_ert ]; then
+    myexec rm "CcspRmSsp $SubsysArgs"
+fi
 
+if [ "x"$1 = "xpam" ] || [ "x"$2 = "xpam" ]; then
+  exit 0
+fi
 
+myexec wecb "CcspWecbController $SubsysArgs" 5
+myexec tr069pa "CcspTr069PaSsp $SubsysArgs" 5
+
+if [ -e ./tr069pa ]; then
+    # add firewall rule to allow incoming packet for port 7547
+    sysevent setunique GeneralPurposeFirewallRule " -A INPUT -i erouter0 -p tcp --dport=7547 -j ACCEPT "
+fi
+
+myexec cm  "CcspCMAgentSsp $SubsysArgs" 5
+myexec mta "CcspMtaAgentSsp $SubsysArgs" 30
+myexec ssd "CcspSsdSsp $SubsysArgs" 1
+myexec fu  "CcspFuSsp $SubsysArgs" 1
+myexec tad "CcspTandDSsp $SubsysArgs" 3
+
+sleep 1
+
+if [ -e ./ccspRecoveryManager ]; then
+    echo "./ccspRecoveryManager $SubsysArgs &"
+    ./ccspRecoveryManager $SubsysArgs &
+fi
+
+myexec lm "CcspLMLite $SubsysArgs" 3
+
+if [ -e ./lm ]; then
+    cd lm
+    sleep 5
+    ./CcspLMLite &
+fi
